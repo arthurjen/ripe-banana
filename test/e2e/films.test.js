19d305c8ffa03cb8add0e66ca6708288a1e4faca
MariahAdams <mariah.adams23@yahoo.com>
2018-07-20 10:07:08 -0700

    completed POST method for films

---
+++ test/e2e/films.test.js
@@ -0,0 +1,95 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./_db');
+
+const { checkOk } = request;
+
+describe.only('Films API', () => {
+
+    beforeEach(() => dropCollection('films'));
+    beforeEach(() => dropCollection('studios'));
+    beforeEach(() => dropCollection('actors'));
+
+    let banks;
+
+    function save(data, resource) {
+        return request
+            .post(`/api/${resource}`)
+            .send(data)
+            .then(checkOk)
+            .then(({ body }) => body);
+    }
+
+    let tom;
+    beforeEach(() => {
+        return save(
+            {
+                name: 'Tom Hanks',
+                dob: new Date(1956, 6, 9),
+                pob: 'Concord, CA'
+            },
+            'actors')
+            .then(checkOk)
+            .then(saved => tom = saved);
+    });
+
+    let disney;
+    beforeEach(() => {
+        return save(
+            {
+                name: 'Disney',
+                address: {
+                    city: 'Burbank',
+                    state: 'California',
+                    country: 'USA'
+                } 
+            },
+            'studios')
+            .then(saved => disney = saved);
+    });
+
+    beforeEach(() => {
+        return save({
+            title: 'Saving Mr. Banks',
+            studio: disney._id,
+            released: 2013,
+            cast: [{
+                role: 'Walt Disney',
+                actor: tom._id
+            }]
+        }, 
+        'films')
+            .then(data => {
+                banks = data;
+            });
+    });
+
+    it('saves a film', () => {
+        assert.isOk(banks._id);
+    });
+
+    //TODO: 
+    it.skip('returns a film on GET', () => {
+        
+    });
+
+    // it('returns all films on GET', () => {
+    //     return request
+    //         .get('/api/films')
+    //         .then(checkOk)
+    //         .then(({ body }) => {
+    //             assert.deepEqual(body, [arthur, mariah]);
+    //         });
+    // });
+
+    // it('updates a film', () => {
+    //     arthur.company = 'Netflix';
+    //     return request
+    //         .put(`/api/films/${arthur._id}`)
+    //         .send(arthur)
+    //         .then(checkOk)
+    //         .then(({ body }) => {
+    //             assert.deepEqual(body, arthur);
+    //         });
+    // });
+});
