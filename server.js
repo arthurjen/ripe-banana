5cb6e89306c1567e2385fdbffc505e3c28c03cac
MariahAdams <mariah.adams23@yahoo.com>
2018-07-23 11:19:14 -0700

    updated README and server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const { createServer } = require('http');
 const app = require('./lib/app');
 const connect = require('../../lib/utils/connect');
-connect('mongodb://localhost:27017/banana');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/banana';
+connect(MONGODB_URI);
 
 const PORT = process.env.PORT || 3000;
 const server = createServer(app);
